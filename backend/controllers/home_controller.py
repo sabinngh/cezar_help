9bab6656e185feeb03c75338ecb9e0cb5e6c06ef
karina <karina@karinas-MacBook-Pro.local>
2026-08-01 13:40:03 +0300

    Fixed all pages navigations

--- backend/controllers/home_controller.py
+++ backend/controllers/home_controller.py
@@ -6,3 +6,9 @@ home_bp = Blueprint("home", __name__)
 @home_bp.route("/")
 def home():
     return render_template("index.html")
+
+
+# Route for Meet the Team page
+@home_bp.route("/team")
+def team():
+    return render_template("page.html")
